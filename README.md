3b9ccf4416768bd6ab965a8dde59ba9925b251d1
Martin Tománek <tomanma9@cvut.cz>
2025-08-14 13:47:03 +0200

    Add ability to downloadVideo given master.json url

---
+++ README.md
@@ -0,0 +1,19 @@
+# Hojnost getter
+
+Scrapes https://www.naplnenahojnost.cz/ for all the video content in there.
+
+## Requirements
+
+- **Scala CLI**: This project requires Scala CLI to be installed
+- **Scala Version**: 3.7.2 (as specified in project.scala)
+- **Java**: Compatible JVM (Java 11+ recommended)
+
+## Installation
+
+1. Install Scala CLI following the [official installation guide](https://scala-cli.virtuslab.org/install)
+2. Clone this repository
+3. Dependencies will be automatically downloaded when you run the project
+
+## Usage
+
+`scala-cli run `
--- project.scala
+++ project.scala
@@ -1,1 +1,47 @@
 //> using scala 3.7.2
+//> using dep com.lihaoyi::os-lib:0.11.5
+//> using dep com.lihaoyi::mainargs:0.7.6
+//> using dep net.ruippeixotog::scala-scraper:3.2.0
+
+import mainargs.*
+import os.*
+
+implicit object PathRead extends TokensReader.Simple[os.Path]:
+  def shortName = "path"
+
+  def read(strings: Seq[String]) = Right(os.Path(strings.head, os.pwd))
+
+type Name = String
+type Url = String
+case class Video(name: Name, masterJsonUrl: Url)
+
+def scrapeRecordings(): Seq[Video] = ???
+def scrapeChapters(): Seq[Video] = ???
+
+/** Does not check for already downloaded videos */
+def downloadVideo(outDir: Path, video: Video, vimeoDlPath: Path): Boolean =
+  val outFile = outDir / video.name / ".mp4"
+  val call = os.call(
+    (vimeoDlPath, "--combine", s"-i ${video.masterJsonUrl}", s"-o $outFile"),
+    stdout = os.Inherit,
+    stderr = os.Inherit
+  )
+
+  if call.exitCode == 0 then return true else false
+
+@main(
+  name = "hojnost getter",
+  doc =
+    "Uses vimeo-dl to download all videos from a given url. Doesn't download already downloaded videos."
+)
+def main(
+  @arg("output directory") outDir: Path,
+  @arg("vimeo-dl path") vimeoDlPath: Path
+): Unit =
+
+  require(os.isDir(outDir), "output directory must be a directory")
+
+  val recordingsFolder = outDir / "live"
+  os.makeDir.all(recordingsFolder)
+  val chaptersFolder = outDir / "chapters"
+  os.makeDir.all(chaptersFolder)
